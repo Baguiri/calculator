571b22b58474da773f966d00a15d8043db1ff187
Baguiri <baguiri9473@gmail.com>
2020-08-27 19:37:31 +0300

    Updates

---
+++ precache-manifest.5be4d718bc902db18807bfa107971182.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef7be51b3de8af87d495b4fb23cd1186",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "82126952c0033d61d0de",
+    "url": "/calculator/static/css/main.626e1fc1.chunk.css"
+  },
+  {
+    "revision": "5ce9ea6d9661d522eec0",
+    "url": "/calculator/static/js/2.e0d7bd38.chunk.js"
+  },
+  {
+    "revision": "01b52e8cfa161811ba4cbd78f485a21f",
+    "url": "/calculator/static/js/2.e0d7bd38.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82126952c0033d61d0de",
+    "url": "/calculator/static/js/main.59970849.chunk.js"
+  },
+  {
+    "revision": "8355966709433f552c3b",
+    "url": "/calculator/static/js/runtime-main.51f5447e.js"
+  }
+]);
